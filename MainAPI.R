e9694c411ce07736b66d951d26b2f327c7128833
George Kampolis <gkampolis@outlook.com>
2019-02-12 17:04:49 +0000

    Added parameter validation.

--- MainAPI.R
+++ MainAPI.R
@@ -1,42 +1,52 @@
 # Intro Comments ----------------------------------------------------------
 
-# This script loads the model and sets up the API. 
-# It assumes that "createModel.R" has already been
-# run and that the model and dataframe are present
-# in the "model" folder. Note that there's no re-
-# striction as to where the model was run, this can
-# be cloned into the docker container along with the
-# model.
+# This script loads the model and sets up the API. It assumes that
+# "createModel.R" has already been run and that the model and dataframe are
+# present in the "model" folder, along with the parameter validation script
+# "valParams.R". Note that there is no restriction as to where the model was
+# run, this can be cloned into the docker container along with the model.
 
-# It is assumed that the script is called from `RunAPI.R`,
-# and that the relevant packages (mlr and plumbr) are loaded.
+# It is assumed that the script is called from `RunAPI.R`, and that the
+# relevant packages (mlr and plumbr) are loaded.
 
 # Created by George Kampolis.
 
 
-# Load Model And Data Set -------------------------------------------------
+# Load Model, Data Set and Validation Function ----------------------------
 
 titanicModel <- readRDS("model/titanicModel.rds")
 
 titanicNewData <- readRDS("model/titanicNewData.rds")
 
 
+source("model/valParams.R")
+
+
 # Prediction API ----------------------------------------------------------
 
 
 #' Predict survival on the titanic
-#' @param pClass Class of passenger. Possible values: 1, 2 or 3.
-#' @param pSex Sex of passenger. Possible values: male or female.
-#' @param pAge Age of passenger in years. Numeric.
-#' @param pFare Fare paid by passenger in £. Numeric
-#' @param pFamily No. of family members on board. Integer.
+#' @param pClass:int Class of passenger. Possible values: 1, 2 or 3.
+#' @param pSex:character Sex of passenger. Possible values: male or female.
+#' @param pAge:numeric Age of passenger in years. Numeric.
+#' @param pFare:numeric Fare paid by passenger in pounds £. Numeric
+#' @param pFamily:int No. of family members on board. Integer.
 #' @get /titanic
 function(pClass, pSex, pAge, pFare, pFamily){
-  titanicNewData$class[1] <<- as.factor(pClass)
-  titanicNewData$sex[1] <<- as.factor(pSex)
-  titanicNewData$age[1] <<- as.numeric(pAge)
-  titanicNewData$fare[1] <<- as.numeric(pFare)
-  titanicNewData$familyOnBoard <<- as.integer(pFamily)
-  pred <- predict(object = titanicModel, newdata = titanicNewData)
-  as.data.frame(pred)
+  
+  # validate parameters passed
+  valResult <- valParams(pClass, pSex, pAge, pFare, pFamily)
+  
+  # make predictions if valid parameters are supplied ----
+  if(isTRUE(valResult)){
+    titanicNewData$class[1] <<- as.factor(pClass)
+    titanicNewData$sex[1] <<- as.factor(pSex)
+    titanicNewData$age[1] <<- as.numeric(pAge)
+    titanicNewData$fare[1] <<- as.numeric(pFare)
+    titanicNewData$familyOnBoard <<- as.integer(pFamily)
+    pred <- predict(object = titanicModel, newdata = titanicNewData)
+    return(as.data.frame(pred))
+  } else {
+    return(valResult)
+  }
 }
---
+++ model/valParams.R
@@ -0,0 +1,81 @@
+# Intro Comments ----------------------------------------------------------
+
+# This script contains a helper function to validate the parameters passed to
+# the model. It should be placed in the model folder, accompanying the actual
+# model and the new data dataframe. The script is sourced (and its function
+# utilised) in the "MainAPI.R"script, in the root folder.
+
+# Created by George Kampolis.
+
+
+# Validate Function -------------------------------------------------------
+
+valParams <- function(pClass, pSex, pAge, pFare, pFamily){
+  # validate parameters received ----
+  ## initialize flags for validation ----
+  correctClass <- FALSE
+  correctSex <- FALSE
+  correctAge <- FALSE
+  correctFare <- FALSE
+  correctFamily <- FALSE
+  
+  ## validate pClass ----
+  if(missing(pClass)){
+    return("Parameter `pClass` is required.")
+  } else if(!all.equal(as.numeric(pClass), as.integer(pClass))){
+    return("Parameter `pClass` must be integer.")
+  } else if(!(as.integer(pClass) %in% c(1,2,3))){
+    return("Parameter `pClass` must be 1, 2 or 3.")
+  } else {
+    correctClass <- TRUE
+  }
+  
+  ## validate pSex ----
+  if(missing(pSex)){
+    return("Parameter `pSex` is required.")
+  } else if(!(as.character(pSex) %in% c("male", "female"))){
+    return("Parameter `pSex` must be either 'male' or 'female'.")
+  } else {
+    correctSex <- TRUE
+  }
+  
+  ## validate pAge ----
+  if(missing(pAge)){
+    return("Parameter `pAge` is required.")
+  } else if(!is.numeric(as.numeric(pAge))){
+    return("Parameter `pAge` must be numeric.")
+  } else if(as.numeric(pAge) < 0 || as.numeric(pAge) > 125){
+    return("Age is expected to be in range of [0,125].")
+  } else {
+    correctAge <- TRUE
+  }
+  
+  ## validate pFare ----
+  if(missing(pFare)){
+    return("Paramer `pFare` is required.")
+  } else if(!is.numeric(as.numeric(pFare))){
+    return("Parameter `pFare` must be numeric.")
+  } else if(as.numeric(pFare)<0 || as.numeric(pFare) >1000){
+    return("Fare is expected to be in range of [0,1000]. Priciest actual fare was 512 pounds.")
+  } else {
+    correctFare <- TRUE
+  }
+  
+  ## validate pFamily ----
+  if(missing(pFamily)){
+    return("Parameter `pFamily` is required.")
+  } else if(!all.equal(as.numeric(pFamily), as.integer(pFamily))){
+    return("Parameter `pFamily` must be integer.")
+  } else if(as.integer(pFamily)<0 || as.integer(pFamily)>50){
+    return("Family members on board is expected to be in range of [0,50].")
+  } else {
+    correctFamily <- TRUE
+  }
+  
+  ## return TRUE if all parameters have been validated succesfully.
+  if(correctClass && correctSex && correctAge && correctFare && correctFamily){
+    return(TRUE)
+  } else {
+    return("Validation error occured.")
+  }
+}
